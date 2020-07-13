ad6fc1d6296ff7012f4fbec510a3403ab5ebd9a4
MitchellGulledge <61255163+MitchellGulledge@users.noreply.github.com>
2020-07-13 07:48:31 -0700

    Update BGP-Aggregation.py
    cleaning up the code so all the user has to enter in is the network name, api key and or name

--- BGP-Aggregation.py
+++ BGP-Aggregation.py
@@ -1,61 +1,71 @@
-import requests
-import json
-import time
+import requests, json, time
 import meraki
-import logging
-import re
-import urllib.request
-import os
-import datetime as dt
-from datetime import datetime, timedelta
 import ast
 from netaddr import *
-import pprint
 
 # Meraki credentials are placed below
 meraki_config = {
 	'api_key': "",
-	'orgName': ""
+	'org_name': ""
 }
 
-# writing function to obtain org ID via linking ORG name
+# this function obtains the org ID by inputting org name and API key
 mdashboard = meraki.DashboardAPI(meraki_config['api_key'])
 result_org_id = mdashboard.organizations.getOrganizations()
 for x in result_org_id:
-    if x['name'] == meraki_config['orgName']:
+    if x['name'] == meraki_config['org_name']:
         meraki_config['org_id'] = x['id']
 
-# branch subnets is a variable to display local branch site info
-branchsubnets = []
-# variable with new and existing s2s VPN config
-merakivpns = []
+# this function maps the network name to obtain the network ID
+def get_net_id(net_name):
+    # performing org wide API call
+    meraki_network_list = mdashboard.networks.getOrganizationNetworks(meraki_config['org_id'])
+    # iterating through list to match network with net_name
+    for networks in meraki_network_list:
+    # if statement matching on the network name and net_name variable user inputs
+        if net_name == networks['name']:
+            # variable to hold the network ID of the specified net name 
+            get_net_id.network_id = networks['id']
 
-# performing initial get to obtain all Meraki existing VPN info to add to merakivpns list above
-originalvpn = mdashboard.organizations.getOrganizationThirdPartyVPNPeers(
-    meraki_config['org_id']
-)
-merakivpns.append(originalvpn)
+# this function performs an org wide Meraki call for all sites VPN statuses
+# not using the SDK for this call as it is currently unavailable for now..
+def org_wide_vpn_status(list_of_meraki_ids):
+    # defining the URL for the GET below
+    org_vpn_url = 'https://api.meraki.com/api/v1/organizations/'\
+        +meraki_config['org_id']+'/appliance/vpn/statuses?networkIds[]='\
+            +(list_of_meraki_ids) # this is the hubs network id
+    # creating the header in order to authenticate the call
+    header = {"X-Cisco-Meraki-API-Key": meraki_config['api_key'], "Content-Type": "application/json"}
+    # performing API call to meraki dashboard
+    vpn_statuses = requests.get(org_vpn_url, headers=header).content
+    # vpn_status is a data type of bytes, going to convert to a string then adictionary
+    decoded_vpn_statuses = vpn_statuses[1:-1].decode("UTF-8") # parsing outer brackets
+    # converting string to dictionary
+    meraki_vpn_peers = ast.literal_eval(decoded_vpn_statuses)
+    # parsing list to just meraki vpn peers
+    list_of_peers = meraki_vpn_peers['merakiVpnPeers']
+    # creating list as placeholder for later call
+    list_of_bgp_peers = []
+    # creating list to hold all Auto VPN subnets connected to this hub
+    list_of_bgp_prefixes = []
+    # iterating through list of peers to create a list of network ids
+    for peers in list_of_peers:
+        # creating variable to match on network ID to later append to list of IDs for org wide call
+        ibgp_peer = peers['networkId']
+        # appending the ibgp_peer to the list_of_bgp_peers list
+        list_of_bgp_peers.append(ibgp_peer)
+        mx_subnets = mdashboard.networks.getNetworkSiteToSiteVpn(ibgp_peer) # gets branch subnets
+        in_meraki_vpn = ([x['localSubnet'] for x in mx_subnets['subnets'] \
+            if x['useVpn'] == True])  # list comprehension to filter for subnets in vpn
+        # concatanating prefix/s to the list list_of_bgp_prefixes
+        list_of_bgp_prefixes = list_of_bgp_prefixes + in_meraki_vpn
+    # summarizing the list of prefixes created
+    summarized_prefixes = cidr_merge(list_of_bgp_prefixes)
+    # creating aggregate address statements from summarized prefix
+    formatted_prefixes = str(summarized_prefixes).replace("IPNetwork('","aggregate-address ")
+    finalized_formatted_prefixes = str(formatted_prefixes).replace ("')", " summary-only")
+    print(finalized_formatted_prefixes)
 
-# Meraki call to obtain Network information
-tagsnetwork = mdashboard.networks.getOrganizationNetworks(meraki_config['org_id'])
-
-# place holder list that prefixes will be appended to
-list_of_prefixes = []
-
-# loop that iterates through the variable tagsnetwork and matches networks with vWAN in the tag
-for i in tagsnetwork:
-    if i['tags'] is None or i['name'] == 'Tag-Placeholder':
-        pass
-    elif "2914:2346" in i['tags']:
-        network_info = i['id'] # need network ID in order to obtain device/serial information
-        netname = i['name'] # network name used to label Meraki VPN and Azure config
-        nettag = i['tags']  # obtaining all tags for network as this will be placed in VPN config
-        va = mdashboard.networks.getNetworkSiteToSiteVpn(network_info) # gets branch local vpn subnets
-        testextract = ([x['localSubnet'] for x in va['subnets']
-						if x['useVpn'] == True])  # list comprehension to filter for subnets in vpn
-        (testextract)
-        print(testextract)
-        list_of_prefixes = list_of_prefixes + testextract
-
-print(list_of_prefixes)
-print(cidr_merge(list_of_prefixes)) # netaddr package autosummarizes prefixes in list of prefixes
+# below is the list of network IDs that are built with the get_meraki_networks_by_tag() function
+get_net_id("enter your dashboard network name here") # statically defining the network name here
+org_wide_vpn_status(get_net_id.network_id)
